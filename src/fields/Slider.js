99f55a6f06990d4c1ab1ffb696d1ea8a9454ebfb
zhipenglin <sunandmoon001@163.com>
2023-09-12 15:40:26 +0800

    修改field不规范问题

--- src/fields/Slider.js
+++ src/fields/Slider.js
@@ -6,10 +6,11 @@ import withItem from "../common/withItem";
 const {useDecorator} = hooks;
 
 const Slider = (props) => {
-  const render = useDecorator(Object.assign({}, props));
-  return render(AntdSlider);
+    const render = useDecorator(Object.assign({}, props));
+    return render(AntdSlider);
 };
 
 Slider.Item = withItem(Slider);
+Slider.Field = AntdSlider;
 
 export default Slider;
