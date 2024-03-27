1e8cfb8ec8c94c04e437fc40f33f23ccff700f65
zhipenglin <sunandmoon001@163.com>
2024-03-27 18:20:02 +0800

    修改withItem传递参数缺失bug

--- src/common/withItem.js
+++ src/common/withItem.js
@@ -3,16 +3,31 @@ import {List, Space} from "@kne/antd-taro";
 import classnames from 'classnames';
 import {View} from '@tarojs/components';
 
-const withItem = (WrappedComponent) => ({labelTips, labelRender, ...props}) => {
+const withItem = (WrappedComponent) => ({
+                                            labelTips,
+                                            labelRender,
+                                            title,
+                                            arrow,
+                                            extra,
+                                            description,
+                                            prefix,
+                                            clickable,
+                                            onClick,
+                                            disabled,
+                                            className,
+                                            ...props
+                                        }) => {
     const listProps = props.labelHidden === true ? {} : {
-        title: <Space>
+        title: title || <Space>
             {typeof labelRender === 'function' ? labelRender(props) : props.label}
             {labelTips && (typeof labelTips === 'function' ? labelTips({labelTips, ...props}) :
                 <View className='label-tips'>{labelTips}</View>)}
         </Space>
     };
     const [isError, setIsError] = useState(false);
-    return <List.Item {...listProps} className={classnames('react-form-list-item', {
+    return <List.Item {...Object.assign({}, listProps, {
+        arrow, extra, description, prefix, clickable, onClick, disabled
+    })} className={classnames('react-form-list-item', className, {
         'is-req': typeof props.rule === 'string' && (props.rule || '').split(' ').indexOf('REQ') > -1,
         'is-error': isError
     })}>
