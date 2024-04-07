538ff1ea4091936804190cd848da54a143ed7a61
zhipenglin <sunandmoon001@163.com>
2024-04-07 17:34:45 +0800

    修改InputNumber

--- src/fields/InputNumber.js
+++ src/fields/InputNumber.js
@@ -1,80 +1,16 @@
-import {View} from "@tarojs/components";
-import Taro from '@tarojs/taro';
-import {Icon, Input as AntdInput} from '@kne/antd-taro';
-import React from "react";
-import useControlValue from '@kne/use-control-value';
-import {isNumber, toNumber} from "lodash";
-import {hooks} from "../helper";
+import {InputNumber} from '@kne/antd-taro';
+import {hooks} from '../helper';
 import withItem from "../common/withItem";
-import classnames from 'classnames';
 
 const {useDecorator} = hooks;
 
-const InputNumberInner = (props) => {
-    const [value, onChange] = useControlValue(props);
-    return <View className={classnames('adm-input-number-wrapper', props.className, {
-        'is-controller-hidden': props.hiddenController
-    })}>
-        <View className={'adm-input-number-left'} onClick={() => {
-            const target = (value && !isNaN(value) ? toNumber(value) : 0) - props.step;
-            if (isNumber(props.min) && target < props.min) {
-                Taro.showToast({
-                    title: `不能小于${props.min}`
-                });
-                return;
-            }
-            onChange(target);
-        }}>
-            <Icon type="arrow-thin-left" className="iconfont"/>
-        </View>
-        <View className={'adm-input-number'}>
-            <AntdInput {...props} type="number" value={value} onChange={(value) => {
-                if (value === '') {
-                    onChange('');
-                    return;
-                }
-                const _value = toNumber(value);
-                if (isNumber(props.min) && _value < props.min) {
-                    Taro.showToast({
-                        title: `不能小于${props.min}`
-                    });
-                    return;
-                }
-                if (isNumber(props.min) && _value > props.max) {
-                    Taro.showToast({
-                        title: `不能大于${props.max}`
-                    });
-                    return;
-                }
-
-                !isNaN(value) && onChange(_value);
-            }}/>
-        </View>
-        <View className={'adm-input-number-right'} onClick={() => {
-            const target = (value && !isNaN(value) ? toNumber(value) : 0) + props.step;
-            if (isNumber(props.min) && target > props.max) {
-                Taro.showToast({
-                    title: `不能大于${props.max}`
-                });
-                return;
-            }
-            onChange(target);
-        }}>
-            <Icon type="arrow-thin-right" className="iconfont"/>
-        </View>
-    </View>
-};
-
-InputNumberInner.defaultProps = {
-    step: 1
-};
-
 const InputNumberField = (props) => {
     const render = useDecorator(Object.assign({placeholder: `请输入${props.label}`}, props));
-    return render(InputNumberInner);
+    return render(InputNumber);
 };
 
 InputNumberField.Item = withItem(InputNumberField);
-InputNumberField.Field = InputNumberInner;
+
+InputNumberField.Field = InputNumber;
 
 export default InputNumberField;
